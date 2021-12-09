f10ff6d0cf83e5ccb75927ff085850448e4927e7
GivensR <48247720+GivensRS@users.noreply.github.com>
2021-12-09 11:52:11 +0300

    Bugfix
    Сделал исключение на ввод данных, чтобы бот не падал от сообщения состоящего из 1 или 2 слов.

--- database.py
+++ database.py
@@ -7,8 +7,13 @@ import telebot
 bot = telebot.TeleBot('1122158709:AAGrNF_UmJD0dt6Tr5Pq6ynYHvs10UkdoLg')
 
 def Pog(message):
-    a  = message.text.split(" ")
-    InputDB.InputDB(a[0], a[1], a[2])
+    try:
+        a  = message.text.split(" ")
+        InputDB.InputDB(a[0], a[1], a[2])
+        bot.reply_to(message, 'Данные были введены')
+    except (IndexError, TypeError, AttributeError):
+        bot.reply_to(message, 'Вы ввели данные не по образцу')
+        get_input_db(message)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -16,26 +21,27 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['inp'])
 def get_input_db(message):
-    a = bot.send_message(message.from_user.id, "Введите далее Страну, тип транспорта и цену поездки для заполнения базы данных.")
+    a = bot.reply_to(message, "Введите далее Страну, тип транспорта и цену поездки для заполнения базы данных.")
     #Следующая команда ждет ответа от пользователя и отсылает этот ответ в функцию Pog(), бот будет ждать жо последнего дня
     bot.register_next_step_handler(a,Pog)
+@bot.message_handler(commands=['db'])
+def give_output_db(message):
+        i = ""
+        b = Output.OutputDB()
+        #Великое чудо Расима, считаю этот код божественным, но сам создатель в ужасе от созданного
+        for d in range(len(b)):
+            i = i + str(d+1) + " "
+            for f in b[d]:
+                i = i + f + " "
+                if f == b[d][-1]:
+                    i += "\n"
+        bot.reply_to(message, text=i)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     global i
     if message.text == "Записать в таблицу":
-        bot.send_message(message.from_user.id,"Введите название страны, средство передвижение и стоимость проезда, для заполнения базы данных с использование /inp")
-    if message.text == "Показать данные из таблицы":
-        i = ""
-        b = Output.OutputDB()
-        #Великое чудо Расима, считаю этот код божественным, но сам создатель в ужасе от созданного
-        for d in range(len(b)):
-            i = i + str(d + 1) + " "
-            for f in b[d]:
-                i = i + f + " "
-                if f == b[d][-1]:
-                    i += "\n"
-        bot.send_message(message.from_user.id, text=i)
+        bot.reply_to(message,"Введите название страны, средство передвижение и стоимость проезда, для заполнения базы данных с использование /inp")
 
 
 bot.polling()
